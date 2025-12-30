d874041236e8ae63453563528247a92b2a09bf44
GitHub Action <action@github.com>
2025-12-30 23:28:08 +0000

    style: auto-format code

--- Megastructure_Game/Assets/Scripts/WorldGenerator.cs
+++ Megastructure_Game/Assets/Scripts/WorldGenerator.cs
@@ -79,7 +79,7 @@ public class WorldGenerator : MonoBehaviour
                     float randomScale = Random.Range(0.25f, 2f) * scale;
                     GameObject structure = Instantiate(structurePrefab, position, Random.rotation, transform);
                     structure.transform.localScale = randomScale * Vector3.one;
-                    
+
 
                     // Adjust Y position so bottom touches the ground
                     Bounds bounds = GetTotalBounds(structure);
@@ -88,7 +88,7 @@ public class WorldGenerator : MonoBehaviour
                         float yOffset = -bounds.min.y;
                         structure.transform.position += Vector3.up * yOffset;
                     }
-                    structure.transform.position -= Vector3.up * Random.Range(2f,10f);
+                    structure.transform.position -= Vector3.up * Random.Range(2f, 10f);
                 }
             }
         }
